acfc721103be3f5d745b4287642c82064f26df8e
mathieu-superpose <m.joly@live.fr>
2025-01-29 23:08:07 +0100

    add circleshape

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -22,6 +24,8 @@ def main():
         screen.fill((14, 14, 14))
 
         pygame.display.flip()
+        
+        dt = clock.tick(FPS) / 1000
 
 
 if __name__ == "__main__":
