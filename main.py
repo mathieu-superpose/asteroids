e359f373a4479572279becbd783b530e3fcc7665
mathieu-superpose <m.joly@live.fr>
2025-01-30 07:25:45 +0100

    add player & draw spaceship

--- main.py
+++ main.py
@@ -4,6 +4,9 @@
 import pygame
 
 from constants import *
+from circleshape import CircleShape
+
+from player import Player
 
 
 def main():
@@ -15,6 +18,8 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -22,9 +27,11 @@ def main():
                 return
 
         screen.fill((14, 14, 14))
+        
+        player.draw(screen)
 
         pygame.display.flip()
-        
+
         dt = clock.tick(FPS) / 1000
 
 
